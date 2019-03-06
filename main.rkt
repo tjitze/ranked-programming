8d091a2354c994b97957bc8c3a8011c9c2e4061b
Tjitze Rienstra <tjitze@gmail.com>
2019-03-06 18:29:44 +0100

    Clean up

--- main.rkt
+++ main.rkt
@@ -1,37 +1,5 @@
 #lang racket/base
 
-;(module+ test
-;  (require rackunit))
-
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
-
-;; Code here
 (require "rp-api.rkt")
-;(require "rp-core.rkt")
 (provide (all-from-out "rp-api.rkt"))
 
-;(module+ test
-;  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-;  ;; or with `raco test`. The code here does not run when this file is
-;  ;; required by another module.
-;
-;  (check-equal? (+ 2 2) 4))
